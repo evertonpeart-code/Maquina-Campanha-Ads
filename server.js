1013a90fce65f41722f1895425f7788e78135adc
evertonpeart-code <evertoncostapeart@gmail.com>
2025-08-04 23:19:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-Commit changes
+# Passo 1: apagar o arquivo
+rm servidor.js
+
+# Passo 2: commitar e enviar pro GitHub
+git add .
+git commit -m "Removendo servidor.js duplicado"
+git push origin main
+
